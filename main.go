68a0d99742e663054782589826f1ca656a7ab70e
marcelobiao <biao.marcelo@gmail.com>
2021-10-16 12:44:02 -0300

    Implementando repository/controller de usuários e conexao com a base de dados mysql

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.Carregar()
 	r := router.Gerar()
+	fmt.Println(config.StringConexaoBanco)
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
