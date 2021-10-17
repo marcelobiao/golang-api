0a2ced3d1494f82e79ecbebd1362920370c650ef
marcelobiao <biao.marcelo@gmail.com>
2021-10-17 11:34:11 -0300

    Implementando rota index de usuários

--- src/modelos/Usuario.go
+++ src/modelos/Usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Usuario struct {
 	ID       uint64    `json:"id,omitempty"`
@@ -8,5 +12,38 @@ type Usuario struct {
 	Nick     string    `json:"nick,omitempty"`
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
-	CriadoEM time.Time `json:"CriadoEm,omitempty"`
+	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
+
+func (usuario *Usuario) Preparar() error {
+
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome é obrigatório e não pode estar em branco")
+	}
+	if usuario.Nick == "" {
+		return errors.New("O nick é obrigatório e não pode estar em branco")
+	}
+	if usuario.Email == "" {
+		return errors.New("O email é obrigatório e não pode estar em branco")
+	}
+	if usuario.Senha == "" {
+		return errors.New("O senha é obrigatório e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
